b353599799eb8116cb83004e845b84992a5d08e6
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-06 14:13:02 -0700

    Remove some state properties and methods and refactored how the Form Link renders to value

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Redirect, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 
 class LogInForm extends Component {
   constructor() {
@@ -8,35 +8,28 @@ class LogInForm extends Component {
       name: '',
       email: '',
       purpose: 'Business',
-      inputIncomplete: true,
       error: ''
     }
   }
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value});
-
   }
 
-  verifyInputs = (e) => {
-    e.preventDefault();
-    if (this.state.name && this.state.email) {
-    	this.setState( { inputIncomplete: false } )
-      this.props.changePath('/areas')
-    } else {
-    	this.setState( { inputIncomplete: true } )
-      this.handleError()
-    }
+  verifyInputs = () => {
+    return this.state.name && this.state.email
   }
 
   handleError = () => {
-    this.state.inputIncomplete && this.setState( { error: 'Please Fill Out All Inputs' } )
+    if (!this.verifyInputs()) {
+      this.setState( { error: 'Please Fill Out All Inputs' } )
+    }
   }
 
   render() {
   	return (
     	<form>
-    	  {this.state.error && <h2> Error! </h2>}
+        {this.state.error && <h2>{this.state.error}</h2>}
         <label for="name">Name:</label>
     	  <input
     	    type="text"
@@ -59,7 +52,8 @@ class LogInForm extends Component {
           <option value="Vacation">Vacation</option>
           <option value="Other">Other</option>
         </select>
-          <button onClick={(e) => this.verifyInputs(e)}>Enter</button>
+        <Link to={() => this.verifyInputs() ? '/areas' : '/'
+        }><span onClick={() => this.handleError()}>Enter</span></Link>
     	</form>
   	)
   }
