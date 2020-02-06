1eb2d286c84b52610a46e2b4a6c3a9c343f9731e
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-05 18:00:18 -0700

    Add changePath prop to change the path when clicking enter button

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Redirect, Link } from "react-router-dom";
 
 class LogInForm extends Component {
   constructor() {
@@ -8,52 +8,59 @@ class LogInForm extends Component {
       name: '',
       email: '',
       purpose: 'Business',
-      inputIncomplete: false
+      inputIncomplete: true,
+      error: ''
     }
   }
 
   handleChange = (e) => {
-    e.preventDefault();
-    this.setState({ [e.target.name]: e.target.value})
+    this.setState({ [e.target.name]: e.target.value});
+
   }
 
-  verifyInputs = () => {
-  	console.log('verify')
-    if (!this.state.name || !this.state.email) {
-    	this.setState( { inputIncomplete: true } )
-    } else {
+  verifyInputs = (e) => {
+    e.preventDefault();
+    if (this.state.name && this.state.email) {
     	this.setState( { inputIncomplete: false } )
+      this.props.changePath('/areas')
+    } else {
+    	this.setState( { inputIncomplete: true } )
+      this.handleError()
     }
-  } 
+  }
+
+  handleError = () => {
+    this.state.inputIncomplete && this.setState( { error: 'Please Fill Out All Inputs' } )
+  }
 
   render() {
   	return (
-  	<form>
-  	  {this.state.inputIncomplete && <h2> Error! </h2>}
-      <label for="name">Name:</label>
-  	  <input
-  	    type="text"
-  	    value={this.state.name}
-  	    placeholder="Enter Your Name"
-  	    name="name"
-        onChange={this.handleChange}
-  	  />
-      <label for="email">Email:</label>
-  	  <input
-  	    type="text"
-  	    value={this.state.email}
-  	    placeholder="Enter Your Email"
-  	    name="email"
-        onChange={this.handleChange}
-  	  />
-      <label for="purpose">Purpose:</label>
-      <select name="purpose" onChange={this.handleChange}>
-        <option value="Business">Business</option>
-        <option value="Vacation">Vacation</option>
-        <option value="Other">Other</option>
-      </select>
-      <button type="button" onClick={() => this.verifyInputs()}>Enter</button>
-  	</form>
+    	<form>
+    	  {this.state.error && <h2> Error! </h2>}
+        <label for="name">Name:</label>
+    	  <input
+    	    type="text"
+    	    value={this.state.name}
+    	    placeholder="Enter Your Name"
+    	    name="name"
+          onChange={this.handleChange}
+    	  />
+        <label for="email">Email:</label>
+    	  <input
+    	    type="text"
+    	    value={this.state.email}
+    	    placeholder="Enter Your Email"
+    	    name="email"
+          onChange={this.handleChange}
+    	  />
+        <label for="purpose">Purpose:</label>
+        <select name="purpose" onChange={this.handleChange}>
+          <option value="Business">Business</option>
+          <option value="Vacation">Vacation</option>
+          <option value="Other">Other</option>
+        </select>
+          <button onClick={(e) => this.verifyInputs(e)}>Enter</button>
+    	</form>
   	)
   }
 }
