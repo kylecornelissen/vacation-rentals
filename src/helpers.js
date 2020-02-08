a1dced72d7c6e5399c6ffe878fd20efdfe9c006b
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-08 14:37:48 -0700

    Change listing key to listings

--- src/helpers.js
+++ src/helpers.js
@@ -2,14 +2,13 @@ export const fetchAreaDetails = (areaData) => {
   let promises = areaData.map(area => {
   	return fetch(`http://localhost:3001${area.details}`)
   	.then(res => res.json())
-    // .then(data => console.log(data))
   	.then(areaDetails => {
   		return {
   			shortName: area.area,
   			name: areaDetails.name,
   			id: areaDetails.id,
   			about: areaDetails.about,
-  			listing: areaDetails.listings
+  			listings: areaDetails.listings
   		}
   	})
   	.catch(error => console.log(error))
