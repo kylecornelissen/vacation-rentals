1f17c4a91a444dcda97707c8ff2197a3857a33a9
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-08 14:38:09 -0700

    Add route for ListingsContainer

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import LogInForm from '../Form/Form.js';
 import AreasContainer from '../AreasContainer/AreasContainer.js';
+import ListingsContainer from '../ListingsContainer/ListingsContainer.js';
 
 export default class App extends Component {
   constructor() {
@@ -18,6 +19,7 @@ export default class App extends Component {
           <Switch>
             <Route exact path="/" component={LogInForm} />
             <Route exact path="/areas" component={AreasContainer} />
+            <Route path="/areas/:id/listings" component={ListingsContainer} />
           </Switch>
         </div>
       </Router>
