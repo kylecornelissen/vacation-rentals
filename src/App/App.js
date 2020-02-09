0888cb5b7efa2ac30ea59a71e5ef462abde70450
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-09 16:18:10 -0700

    Add Listing path to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LogInForm from '../Form/Form.js';
 import AreasContainer from '../AreasContainer/AreasContainer.js';
 import ListingsContainer from '../ListingsContainer/ListingsContainer.js';
 import Navigation from '../Navigation/Navigation.js'
+import Listing from '../Listing/Listing.js'
 
 export default class App extends Component {
   constructor() {
@@ -22,10 +23,10 @@ export default class App extends Component {
         <div className="App">
           <Navigation name={this.state.name} purpose={this.state.purpose}/>
           <Switch>
-            <Route path="/areas/:id/listings" component={ListingsContainer} />
+            <Route exact path="/areas/:area_id/listings" component={ListingsContainer} />
             <Route exact path='/' render={() => <LogInForm addUser={this.addUser} />} />
             <Route exact path='/areas' render={() => <AreasContainer addUser={this.addUser}/>} />
-
+            <Route exact path='/areas/:area_id/listings/:listing_id' component={Listing} />
           </Switch>
         </div>
       </Router>
