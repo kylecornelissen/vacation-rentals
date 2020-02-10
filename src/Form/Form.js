71c9beac44355a7ec96c6c9fb1950d6b9ddac99a
Jesse Maxim <jessmaxim303@gmail.com>
2020-02-09 23:31:48 -0700

    Added more styling for new back pages

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import './Form.css';
 
 class Form extends Component {
   constructor() {
@@ -37,32 +38,40 @@ class Form extends Component {
   	return (
     	<form>
         {this.state.error && <h2>{this.state.error}</h2>}
-        <label for="name">Name:</label>
-    	  <input
-    	    type="text"
-    	    value={this.state.name}
-    	    placeholder="Enter Your Name"
-    	    name="name"
-          onChange={this.handleChange}
-    	  />
-        <label for="email">Email:</label>
-    	  <input
-    	    type="text"
-    	    value={this.state.email}
-    	    placeholder="Enter Your Email"
-    	    name="email"
-          onChange={this.handleChange}
-    	  />
-        <label for="purpose">Purpose:</label>
-        <select name="purpose" onChange={this.handleChange}>
-          <option value="Business">Business</option>
-          <option value="Vacation">Vacation</option>
-          <option value="Other">Other</option>
-        </select>
-        <Link to={() => this.verifyInputs() ? '/areas' : '/'}><span onClick={() => this.handleError()}>Enter</span></Link>
-    	</form>
-  	)
-  }
+        <div className="form_container">
+          <article className="form_input-container">
+            <label for="name">Name:</label>
+    	      <input
+    	        type="text"
+    	        value={this.state.name}
+    	        placeholder="Enter Your Name"
+    	        name="name"
+              onChange={this.handleChange}
+    	      />
+          </article>
+          <article className="form_input-container">
+          <label for="email">Email:</label>
+    	    <input
+    	      type="text"
+    	      value={this.state.email}
+    	      placeholder="Enter Your Email"
+    	      name="email"
+            onChange={this.handleChange}
+    	    />
+          </article>
+          <article className="form_input-container">
+            <label for="purpose">Purpose:</label>
+            <select name="purpose" onChange={this.handleChange}>
+              <option value="Business">Business</option>
+              <option value="Vacation">Vacation</option>
+              <option value="Other">Other</option>
+            </select>
+          </article>
+          <Link to={() => this.verifyInputs() ? '/areas' : '/'}><button className="main_button" onClick={() => this.handleError()}>Enter</button></Link>
+          </div>
+      	</form>
+    	)
+    }
 }
 
 export default Form;
